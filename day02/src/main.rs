ab0872be0f917321cc76c082ae0d5fb3a3837f06
Ronald Chiang <z5419773@ad.unsw.edu.au>
2022-12-02 19:37:29 +1100

    made scores into tuples and save lines

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -7,26 +7,17 @@ fn main() {
     let mut line = String::new();
     let _len = input.read_to_string(&mut line);
     
-    let score: u32 = line
+    let individual_scores = line
         .split('\n')
         .into_iter()
         .map(|rpsmatch| {
             let o = rpsmatch.split(' ').nth(0).unwrap().chars().next().unwrap() as u32;
             let u = rpsmatch.split(' ').nth(1).unwrap().chars().next().unwrap() as u32;
-            u - 'W' as u32 + (u - o + 2).rem_euclid(3) * 3
-        })
-        .fold(0, |a, b| a + b);
-
-    let score2: u32 = line
-        .split('\n')
-        .into_iter()
-        .map(|rpsmatch| {
-            let o = rpsmatch.split(' ').nth(0).unwrap().chars().next().unwrap() as u32;
-            let u = rpsmatch.split(' ').nth(1).unwrap().chars().next().unwrap() as u32;
-            (u - 'X' as u32) * 3 + (o - 'A' as u32 + u - 'X' as u32 + 2).rem_euclid(3) + 1
-        })
-        .fold(0, |a, b| a + b);
-
-    println!("The total score for part 1 was {}", score);
-    println!("The total score for part 2 was {}", score2);
+            (u - 'W' as u32 + (u - o + 2).rem_euclid(3) * 3,
+            (u - 'X' as u32) * 3 + (o - 'A' as u32 + u - 'X' as u32 + 2).rem_euclid(3) + 1)
+        });
+    
+    let scores = individual_scores.fold([0,0], |a, b| [a[0] + b.0, a[1] + b.1]);
+    println!("The total score for part 1 was {}", scores[0]);
+    println!("The total score for part 2 was {}", scores[1]);
 }
