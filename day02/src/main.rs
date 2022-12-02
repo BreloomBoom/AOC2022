51e4dec9bb105e529c6ece41e6175bf079253b13
Ronald Chiang <z5419773@ad.unsw.edu.au>
2022-12-03 08:30:52 +1100

    day 2 cleaned up and even less readable

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,13 +1,8 @@
-use std::io::BufReader;
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 fn main() {
-    let mut input = BufReader::new(File::open("input.txt").expect("File could not open"));
-    let mut line = String::new();
-    let _len = input.read_to_string(&mut line);
-    
-    let individual_scores = line
+    let scores = fs::read_to_string("input.txt")
+        .unwrap()
         .split('\n')
         .into_iter()
         .map(|rpsmatch| {
@@ -15,9 +10,9 @@ fn main() {
             let u = rpsmatch.split(' ').nth(1).unwrap().chars().next().unwrap() as u32;
             (u - 'W' as u32 + (u - o + 2).rem_euclid(3) * 3,
             (u - 'X' as u32) * 3 + (o - 'A' as u32 + u - 'X' as u32 + 2).rem_euclid(3) + 1)
-        });
-    
-    let scores = individual_scores.fold([0,0], |a, b| [a[0] + b.0, a[1] + b.1]);
+        })
+        .fold([0,0], |a, b| [a[0] + b.0, a[1] + b.1]);
+
     println!("The total score for part 1 was {}", scores[0]);
     println!("The total score for part 2 was {}", scores[1]);
 }
