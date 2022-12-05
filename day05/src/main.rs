655b90770c4b9758e12955cd846ee64e92ad164d
Ronald Chiang <z5419773@ad.unsw.edu.au>
2022-12-05 20:03:14 +1100

    day 5 done

---
+++ day05/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use std::collections::LinkedList;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+ 
+    println!("The top crates are {}", sol(input.clone()));
+}
+
+fn sol(input: String) -> String {
+    let mut crates_commands = input.split("\n\n");
+    let unparsed_crates = crates_commands.next().unwrap();
+    let mut crates = parse_crates(unparsed_crates.clone());
+    let mut crates2 = parse_crates(unparsed_crates);
+
+    for command in crates_commands.next().unwrap().split('\n') {
+        let mut parts = command.split(" from ");
+        let crate_no = (parts.next().unwrap())[5..].parse::<i32>().unwrap();
+
+        let from_to = parts.next().unwrap();
+        let from = from_to[0..1].parse::<usize>().unwrap();
+        let to = from_to[5..6].parse::<usize>().unwrap();
+
+        let mut stack = LinkedList::new();
+        for _ in 0..crate_no {
+            let single_crate = (crates[from - 1]).pop_back().unwrap();
+            stack.push_back(single_crate.clone());
+            (crates[to - 1]).push_back(single_crate);
+        }
+        for _ in 0..crate_no {
+            (crates2[to - 1]).push_back(stack.pop_back().unwrap());
+        }
+    }
+
+    let mut answer = crates
+        .into_iter()
+        .map(|crate_list| *(crate_list.back().unwrap()))
+        .collect::<String>();
+    answer.push_str(" and ");
+    answer.push_str(crates2
+        .into_iter()
+        .map(|crate_list| *(crate_list.back().unwrap()))
+        .collect::<String>().as_str());
+    answer
+}
+
+fn parse_crates(unparsed_crates: &str) -> Vec<LinkedList<char>> {
+    let mut crates: Vec<LinkedList<char>> = vec![LinkedList::new(); 9];
+    for row in unparsed_crates.split('\n') {
+        for i in 0..9 {
+            let character = row[(1 + 4 * i)..(2 + 4 * i)].chars().next().unwrap();
+            if character != ' ' {
+                (crates[i]).push_front(character);
+            }
+        }
+    }
+    crates
+}
