f277b5110bbd5e97a308dbe04d539b6d2aae634e
Ronald Chiang <z5419773@ad.unsw.edu.au>
2022-12-06 18:06:15 +1100

    day 6 done

---
+++ day06/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    println!("The first start-of-packet marker is detected at {}", part1(&input).unwrap());
+    println!("The first start-of-packet marker is detected at {}", part2(&input).unwrap());
+}
+
+fn part1(input: &str) -> Option<usize> {
+    let len = input.len();
+    for i in 0..len-3 {
+        if unique(&input[i..i+4]) == None {
+            return Some(i+4)
+        }
+    }
+    None
+}
+
+fn part2(input: &str) -> Option<usize> {
+    let len = input.len();
+    for i in 0..len-3 {
+        if unique(&input[i..i+14]) == None {
+            return Some(i+14)
+        }
+    }
+    None
+}
+
+fn unique(s: &str) -> Option<(usize, usize, char)> {
+    s.chars().enumerate().find_map(|(i, c)| {
+        s.chars()
+            .enumerate()
+            .skip(i + 1)
+            .find(|(_, other)| c == *other)
+            .map(|(j, _)| (i, j, c))
+    })
+}
