3c6eaff35db6919950041716d31d08ed313c2a79
Ronald Chiang <z5419773@ad.unsw.edu.au>
2022-12-06 18:22:36 +1100

    susinct part 1 sol

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -3,25 +3,21 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
 
-    println!("The first start-of-packet marker is detected at {}", part1(&input).unwrap());
+    println!("The first start-of-packet marker is detected at {}", part1(&input));
     println!("The first start-of-packet marker is detected at {}", part2(&input).unwrap());
 }
 
-fn part1(input: &str) -> Option<usize> {
+fn part1(input: &str) -> usize {
     let len = input.len();
-    for i in 0..len-3 {
-        if unique(&input[i..i+4]) == None {
-            return Some(i+4)
-        }
-    }
-    None
+    (0..len - 3).filter(|i| unique(&input[*i..i + 4]).is_none()).next().unwrap() + 4
 }
+
 
 fn part2(input: &str) -> Option<usize> {
     let len = input.len();
-    for i in 0..len-3 {
-        if unique(&input[i..i+14]) == None {
-            return Some(i+14)
+    for i in 0..len - 3 {
+        if unique(&input[i..i + 14]) == None {
+            return Some(i + 14)
         }
     }
     None
